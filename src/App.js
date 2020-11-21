54f8f7648aae8ec01024b200b45fb8f65962d95a
Hemanth R <hemanthrt18@gmail.com>
2020-11-21 17:13:36 +0530

    Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+            <Route path="/team">
+            </Route>
+        </Switch>
+      </Router>
+      
       <section className="landing">
       <Navbar />
         <Button variant="success">
